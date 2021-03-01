5443d964669ddcbf63bf4f6c5f4198b8ee419d24
esteban.toro <esteban.toro@kellogg.northwestern.edu>
2021-02-28 20:04:06 -0600

    a

--- movies.js
+++ movies.js
@@ -1,6 +1,29 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
-    let db = firebase.firestore()
-    let apiKey = 'your TMDB API key'
+firebase.auth().onAuthStateChanged(async function(user) {
+  
+    if (user) {
+      // Signed in
+      let db = firebase.firestore()
+  
+      db.collection('users').doc(user.uid).set({
+        name: user.displayName,
+        email: user.email
+      })
+
+document.querySelector('.sign-in-or-sign-out').innerHTML = `
+<div class="w-1/2 p-4 mx-auto">
+          <a href="#" class="text-center text-white mt-4 px-4 py-2 rounded">Signed in as ${user.displayName}
+        </a>
+        </div>
+        <button class="text-pink-500 underline sign-out">Sign Out</button>
+`
+
+document.querySelector('.sign-in-or-sign-out').addEventListener('click', function(event){
+event.preventDefault
+firebase.auth().signOut()
+document.location.href = 'movies.html'
+})
+  
+    let apiKey = 'ba947c3be02a546650c19d932cc0a805'
     let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
     let json = await response.json()
     let movies = json.results
@@ -8,28 +31,58 @@ window.addEventListener('DOMContentLoaded', async function(event) {
     
     for (let i=0; i<movies.length; i++) {
       let movie = movies[i]
-      let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+      let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
       let watchedMovie = docRef.data()
       let opacityClass = ''
       if (watchedMovie) {
         opacityClass = 'opacity-20'
       }
-  
       document.querySelector('.movies').insertAdjacentHTML('beforeend', `
         <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
           <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
         </div>
       `)
-  
       document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
         event.preventDefault()
-        let movieElement = document.querySelector(`.movie-${movie.id}`)
-        movieElement.classList.add('opacity-20')
-        await db.collection('watched').doc(`${movie.id}`).set({})
-      }) 
+        if (document.querySelector(`.movie-${movie.id}`).classList.contains('opacity-20') == true) {
+          document.querySelector(`.movie-${movie.id}`).classList.remove(`opacity-20`)
+          await db.collection(`watched`).doc(`${movie.id}-${user.uid}`).delete()
+        }
+        else {
+          document.querySelector(`.movie-${movie.id}`).classList.add(`opacity-20`)
+          await db.collection(`watched`).doc(`${movie.id}-${user.uid}`).set({})
+        }
+        })
+      }
     }
-  })
+      else {
+        let ui = new firebaseui.auth.AuthUI(firebase.auth())
+        let authConfig = {
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            signInSuccessUrl: 'movies.html'
+            }
+            ui.start('.sign-in-or-sign-out', authConfig)
+      }
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   
   // Goal:   Refactor the movies application from last week, so that it supports
   //         user login and each user can have their own watchlist.
@@ -48,6 +101,11 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   //         in as <name>" along with a link to "Sign out". Ensure that a document
   //         is set in the "users" collection for each user that signs in to 
   //         your application.
+
+
+
+
+
   // Step 3: Setting the TMDB movie ID as the document ID on your "watched" collection
   //         will no longer work. The document ID should now be a combination of the
   //         TMDB movie ID and the user ID indicating which user has watched. 
